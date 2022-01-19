18e865381cf9e56d0507f8566e23fccef82fe247
Alberto Chávez [GDL] <dchavez@compusoluciones.com>
2022-01-19 15:21:21 -0600

    Add pwa

--- next.config.js
+++ next.config.js
@@ -1,20 +1,34 @@
-module.exports = {
+const withPWA = require('next-pwa');
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    mode: 'production',
+    disable: false,
+  },
   reactStrictMode: true,
   images: {
     domains: ['placeimg.com'],
   },
-  // env: {
-  //   customKey: 'customValue',
-  // },
-  // basePath: '/dist',
-  // compress: true,
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/hola',
-  //       destination: 'https://gndx.dev',
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
-};
+});
+
+// module.exports = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ['placeimg.com'],
+//   },
+// env: {
+//   customKey: 'customValue',
+// },
+// basePath: '/dist',
+// compress: true,
+// async redirects() {
+//   return [
+//     {
+//       source: '/hola',
+//       destination: 'https://gndx.dev',
+//       permanent: true,
+//     },
+//   ];
+// },
+// };
