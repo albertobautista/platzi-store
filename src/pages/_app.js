4e71f43dfda21cbc303b2e1d6277255098e3942b
Alberto Chávez [GDL] <dchavez@compusoluciones.com>
2022-01-19 12:51:33 -0600

    first commit

---
+++ src/pages/_app.js
@@ -0,0 +1,27 @@
+import Script from 'next/script';
+import AppContext from '@context/AppContext';
+import useInitialState from '@hooks/useInitialState';
+import Header from '@components/Header';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  const initialState = useInitialState();
+  return (
+    <AppContext.Provider value={initialState}>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-BERS9MRYF1"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());              
+                gtag('config', 'G-BERS9MRYF1');`}
+      </Script>
+      <Header />
+      <Component {...pageProps} />
+    </AppContext.Provider>
+  );
+}
+
+export default MyApp;
